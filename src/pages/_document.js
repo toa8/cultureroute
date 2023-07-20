b963063c78f1fec5d8aadefdcc9036ed297192ca
Talha Orhun Akdas <talhaorhunakdas@gmail.com>
2023-07-20 16:16:20 +0300

    rmv: googleAdsenseLink

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,13 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7239635439822897"
-          crossOrigin="anonymous"
-        ></script>
-      </Head>
+      <Head></Head>
       <body>
         <Main />
         <NextScript />

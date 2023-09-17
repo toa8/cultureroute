9a42ade7225eed616452985e946b7a0ec559b0de
Talha Orhun Akdas <talhaorhunakdas@gmail.com>
2023-09-17 14:23:45 +0300

    feat: added google adsense

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head></Head>
+      <Head>
+        <script
+          async
+          src={process.env.NEXT_PUBLIC_GOOGLE_ADSENSE}
+          crossorigin="anonymous"
+        ></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
